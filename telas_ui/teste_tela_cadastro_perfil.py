9cdd352a93b6ee983af4d92b682bee60d09d29d8
pliniocefet <plinio.cefet@gmail.com>
2020-11-28 10:43:30 -0300

    testando PyQt5

--- telas_ui/teste_tela_cadastro_perfil.py
+++ telas_ui/teste_tela_cadastro_perfil.py
@@ -1,5 +1,6 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
 
 from ui_tela_cadastro_perfil import Ui_tela_cadastro_perfil
 
@@ -8,13 +9,14 @@ class TestePerfil(QtWidgets.QMainWindow, Ui_tela_cadastro_perfil):
     def __init__(self):
         super(TestePerfil, self).__init__()
         self.setupUi(self)
+        self.app = None
+        self.tela_cadastro_perfil = None
 
-
-
-
-if __name__ == "__main__":
-    import sys
-    app= QtWidgets.QApplication(sys.argv)
-    tela_cadastro_perfil = TestePerfil()
-    tela_cadastro_perfil.show()
-    sys.exit(app.exec_())
+    
+    def chama_tela_teste_perfil(self):
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.tela_cadastro_perfil = TestePerfil()
+        tela_cadastro_perfil.show()
+        sys.exit(app.exec_())
+    
+chama = TestePerfil().chama_tela_teste_perfil()
